f22458b01f0c885de1fc92879cb1cfdd900db2dd
SBENTCODES <147941416+SBENTCODES@users.noreply.github.com>
2024-06-19 12:38:27 +0530

    logo adjusted

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({ page }) => {
   return (
     <div className="bg-slate-800">
       <header class="relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 text-white md:mx-auto md:flex-row md:items-center">
-        <img className="h-16" src={logo} alt={"Mapwala"} />
+        <img className=" h-16 w-64" src={logo} alt={"Mapwala"} />
         <input type="checkbox" class="peer hidden" id="navbar-open" />
         <label
           class="absolute top-5 right-7 cursor-pointer md:hidden"
@@ -33,17 +33,17 @@ const Navbar = ({ page }) => {
           class="flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all peer-checked:mt-8 peer-checked:max-h-56 md:ml-24 md:max-h-full md:flex-row md:items-start"
         >
           <ul class="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
-            <li class={`${page == "home" ? "font-bold" : ""} md:mr-12`}>
+            <li class={`${page === "home" ? "font-bold" : ""} md:mr-12`}>
               <Link to="/">Home</Link>
             </li>
-            <li class={`${page == "about" ? "font-bold" : ""} md:mr-12`}>
+            <li class={`${page === "about" ? "font-bold" : ""} md:mr-12`}>
               <Link to="/about">About Us</Link>
             </li>
-            <li class={`${page == "services" ? "font-bold" : ""} md:mr-12`}>
+            <li class={`${page === "services" ? "font-bold" : ""} md:mr-12`}>
               <Link to="/services">Products & Services</Link>
             </li>
             
-            <li class={`${page == "contact" ? "font-bold" : ""} md:mr-12`}>
+            <li class={`${page === "contact" ? "font-bold" : ""} md:mr-12`}>
               <Link to="/contact">Contact</Link>
             </li>
           </ul>
